65b77a08c215838acf360802e578f57c017e9fbf
Yuanzidan081 <1579417227@qq.com>
2023-11-04 22:55:01 +0800

    Tutorial 1 and 2

---
+++ tutorial1/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
+
+/*
+Prereqs:
+- basic knowledge of C++ (pointers and memory management)
+- very basic knowledge of Qt (Widgets)
+Tutorial Topics:
+- QGraphicsScene
+- QGraphicsItem (QGraphicsRectItem)
+- QGraphicsView
+*/
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // Create a scene
+    QGraphicsScene* scene = new QGraphicsScene();
+
+    // Create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+
+    rect->setRect(0,0,100,100);
+
+    // Add the item to the scene
+    scene->addItem(rect);
+
+    // Add a view
+    // You can also write the code as following
+    /*
+       QGraphicsView* View = new QGraphicsView();
+       View->setScene(scene);
+    */
+    QGraphicsView* View = new QGraphicsView(scene);
+
+
+    View->show();
+    return a.exec();
+}
---
+++ tutorial2/main.cpp
@@ -0,0 +1,42 @@
+﻿#include <QApplication>
+#include <QGraphicsScene>
+#include "MyRect.h"
+#include <QGraphicsView>
+
+/*
+Tutorial Topics:
+- events ((keyPressEvent) and QKeyEvent)
+- event propogation system
+- QDebug
+*/
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+
+  // Create a scene
+  QGraphicsScene *scene = new QGraphicsScene();
+
+  // Create an item to put into the scene
+  MyRect *rect = new MyRect();
+
+  rect->setRect(0, 0, 100, 100);
+
+  // Add the item to the scene
+  scene->addItem(rect);
+
+  // make rect focusable
+  rect->setFlag(QGraphicsItem::ItemIsFocusable);
+  rect->setFocus();
+
+  // Add a view
+  // You can also write the code as following
+  /*
+     QGraphicsView* View = new QGraphicsView();
+     View->setScene(scene);
+  */
+  QGraphicsView *View = new QGraphicsView(scene);
+
+  View->show();
+  return a.exec();
+}
